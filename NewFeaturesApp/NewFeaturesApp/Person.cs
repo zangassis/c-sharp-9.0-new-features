f1b6c82245f2417e1aed9b50a347cf4c8ceb3828
Assis Zang <assis.compasso@dotzmkt.com.br>
2021-07-09 11:05:37 -0300

    feat: Create project

---
+++ NewFeaturesApp/NewFeaturesApp/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewFeaturesApp
+{
+    // With 'init' it will give error if we will try to set property value
+    // anywhere else except constructor or initializer
+    public class Person
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string LastName { get; init; }
+    }
+}
---
+++ NewFeaturesApp/NewFeaturesApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using NewFeaturesApp;
+using System;
+
+// Top-level statements
+// With C # 9.0, we can start writing declarations directly, without the need for namespaces, classes etc...
+
+// Logical Patterns
+// You can combine relational operators with Logical Operators and, or and not(word)
+string namePerson = "John";
+
+if (namePerson is not null)
+{
+    Console.WriteLine($"The name of the person is: {namePerson}, and his complete name is: {CompleteNamePerson(namePerson)}");
+}
+
+Console.WriteLine($"The result of Add Numbers is: {AddNumbers(5, 5)}");
+
+Console.WriteLine($"The temperature is: { CalculateTemperature(-1)}");
+
+static double AddNumbers(int firstNumber, int secondNumber)
+{
+    return firstNumber + secondNumber;
+}
+
+static string CompleteNamePerson(string namePerson)
+{
+    string CompleteNamePerson = string.Empty;
+
+    // Target - typed "new"
+    // Expression doesn't need to say type it will come from context
+    // Property values ​​are set in the constructor
+    Person person = new() { Id = Guid.NewGuid(), Name = namePerson, LastName = "Smith" };
+
+    CompleteNamePerson = person.Name + " " + person.LastName;
+
+    return CompleteNamePerson;
+}
+
+// New switch sintaxe and Relational Patterns
+// Relational operators (<, >, etc) can be used as patterns indicators
+static string CalculateTemperature(int temperature)
+{
+    string resultTemperature = temperature switch
+    {
+        > 0 => "cold",
+        <= -1 => "freezing",
+        _ => throw new ArgumentException("Not a known Product type", nameof(temperature)),
+    };
+
+    return resultTemperature;
+}
